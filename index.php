9460976dd56cc2cdf92d9e8d7fa8e3c85ceb3f79
bernard-ng <ngandubernard@gmail.com>
2018-12-20 01:25:21 +0200

    [Update] run the photoserver and update his options

--- index.php
+++ index.php
@@ -1,35 +1,5 @@
 <?php
-
-$mainDir = new DirectoryIterator(dirname(__DIR__, 3) . "Users\Bernard-ng\pictures\NGPICTURES");
-$photosDir = [];
-
-foreach ($mainDir as $dir) {
-	if ($dir->getFilename() != '.' && $dir->getFilename() != '..' && !empty($dir->getFilename())) {
-		$photosDir[] = $dir->getPathname();
-	}
-}
-
-$sizeLimitDir = count($photosDir) - 1;
-
-
-function get(int $indexDir, int $indexFile, array $photosDir, int $sizeLimitDir)
-{
-	global $sizeLimitDir;
-	global $photosDir;
-
-	$indexDir = ($indexDir > $sizeLimitDir) ? $sizeLimitDir : ($indexDir < 0) ? 0 : $indexDir;
-	$photoDir = $photosDir[$indexDir];
-	$files = [];
-
-	foreach (new DirectoryIterator($photoDir) as $photo) {
-		if ($photo->getFilename() != '.' && $photo->getFilename() != '..') {
-			$files[] = $photo->getPathname();
-		}
-	}
-
-	$file = $files[$indexFile] ?? $files[0] ?? null;
-	$file = require($file);
-}
-
-
-get(7, 0);
+require("vendor/autoload.php");
+$main = dirname(__DIR__, 3) . "Users\\Bernard-ng\\pictures\\NGPICTURES";
+$server = new \Ng\Photoserver\Server($main);
+$server->render(2, 4);
