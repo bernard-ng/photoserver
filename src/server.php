acf6c638c35a438d8c6ae30bd1a4481da76bdb9a
bernard-ng <ngandubernard@gmail.com>
2018-12-19 23:49:03 +0200

    [Update] restructure

--- src/server.php
+++ src/server.php
@@ -2,9 +2,17 @@
 namespace Ng\Photoserver;
 
 
-
+/**
+ * Class Server
+ * @package Ng\Photoserver
+ */
 class Server 
 {
+
+    /**
+     * Server constructor.
+     * @param string $path
+     */
     public function __construct(string $path)
     {
         $this->path = $path;
